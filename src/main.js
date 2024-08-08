3fed7d62ab7e1060857b4dcfe6680eb25c222448
uncledon.cn <dongyf@www.uncledon.cn>
2024-08-08 09:52:14 +0800

    Add sitemap.xml

--- src/main.js
+++ src/main.js
@@ -33,6 +33,32 @@ router.beforeEach((to, from, next) => {
     }
 })
 
+function getRoutesList(routes, pre){
+    return routes.reduce((array, route) =>{
+        const path = `${pre}${route.path}`
+        if (route.path !== '*'){
+            array.push(path)
+        }
+
+        if (route.children){
+            array.push(...getRoutesList(route.children, `${path}/`))
+        }
+
+        return array
+    }, [])
+}
+
+// eslint-disable-next-line
+function getRoutesXML(){
+    const list = getRoutesList(router.options.routes, 'http://www.uncledon.cn')
+        .map(route => `<url><loc>${route}</loc></url>`)
+        .join('\r\n');
+    return `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+    ${list}
+  </urlset>`;
+}
+
+// console.log(getRoutesXML())
 
 app.use(router)
 app.mount('#app')
